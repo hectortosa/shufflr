c42eb2bfeea5dad94ae0bf2cd05161deac19251e
Héctor Tortosa <hector.tortosa@unit4.com>
2017-11-09 18:46:16 +0000

    Shuffle list method

---
+++ shufflr.js
@@ -0,0 +1,20 @@
+exports.shuffle = function (list, seed) {
+    var shuffleItem,
+        i, j;
+
+    if (list.length <= 2) {
+        return list;
+    }
+
+    seed = seed || 10000
+
+    for (i = 0; i < list.length - 2; i++) {
+        j = (Math.round(Math.random() * seed) + i) % list.length;
+
+        shuffleItem = list[i];
+        list[i] = list[j];
+        list[j] = shuffleItem;
+    }
+
+    return list;
+}
