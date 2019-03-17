1ce66b464534c5004040cfdb0df8f47760444295
Héctor Tortosa <hectortosa@icloud.com>
2019-03-17 11:56:05 +0100

    Return shuffled array as a new array instead of modify passing parameter array

--- shufflr.js
+++ shufflr.js
@@ -1,23 +1,25 @@
 export function shuffle(list, seed) {
-  var shuffleItem, i, j;
+  var shuffleItem, i, j, shuffledList;
 
   if (!Array.isArray(list)) {
     return [];
   }
 
-  if (list.length <= 2) {
+  shuffledList = Array.from(list);
+
+  if (shuffledList.length <= 2) {
     return list;
   }
 
   seed = seed || 10000;
 
-  for (i = 0; i < list.length - 2; i++) {
-    j = (Math.round(Math.random() * seed) + i) % list.length;
+  for (i = 0; i < shuffledList.length - 2; i++) {
+    j = (Math.round(Math.random() * seed) + i) % shuffledList.length;
 
-    shuffleItem = list[i];
-    list[i] = list[j];
-    list[j] = shuffleItem;
+    shuffleItem = shuffledList[i];
+    shuffledList[i] = list[j];
+    shuffledList[j] = shuffleItem;
   }
 
-  return list;
+  return shuffledList;
 }
--- test/test.js
+++ test/test.js
@@ -8,5 +8,11 @@ describe("shufflr", function() {
 
       expect(shuffle(notArray)).to.be.an("array").that.is.empty;
     });
+
+    it("should not modify parameter array and return a copy of it shuffled", function() {
+      var list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+      expect(shuffle(list)).to.be.not.equal(list);
+    });
   });
 });
