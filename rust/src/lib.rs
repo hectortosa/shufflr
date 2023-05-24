b53ca37774b1930cb80c6bb52692a48c052c8134
Héctor Tortosa <hectortosa@icloud.com>
2023-05-25 01:34:44 +0300

    Added docs, set toolchain, remove the need of seed

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,22 +1,42 @@
 use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
 
-pub fn shuffle<T: Clone>(list: &[T], seed: u64) -> Vec<T> {
+/// Shuffles a slice of generic type `T` using the Fisher-Yates shuffle algorithm.
+///
+/// # Arguments
+///
+/// * `list` - A slice of generic type `T` to be shuffled.
+/// * `seed` - A `u64` seed value used to initialize the random number generator.
+///
+/// # Returns
+///
+/// A new `Vec<T>` that is a shuffled version of the input slice.
+///
+/// # Examples
+///
+/// ```
+/// let list = vec![1, 2, 3, 4, 5];
+///
+/// let shuffled_list = shufflr::shuffle(&list);
+///
+/// assert_ne!(list, shuffled_list);
+/// ```
+pub fn shuffle<T: Clone>(list: &[T]) -> Vec<T> {
     let mut shuffled_list = Vec::from(list);
     
     if shuffled_list.len() <= 2 {
         return shuffled_list;
     }
     
-    let mut rng = SmallRng::seed_from_u64(seed);
+    let mut rng = SmallRng::from_entropy();
     
-    for i in 0..(shuffled_list.len() - 2) {
-        let j = (rng.gen_range(i..shuffled_list.len()) + i) % shuffled_list.len();
+    for i in (1..shuffled_list.len()).rev() {
+        let j = rng.gen_range(0..=i);
         
         shuffled_list.swap(i, j);
     }
     
-    shuffled_list
+    return shuffled_list
 }
 
 #[cfg(test)]
@@ -28,7 +48,7 @@ mod tests {
     #[test]
     fn should_not_modify_parameter_array_and_return_a_copy_of_it_shuffled() {
         let list = vec![1, 2, 3, 4, 5];
-        let shuffled = shuffle(&list, 100);
+        let shuffled = shuffle(&list);
         assert_ne!(list, shuffled);
         assert_eq!(list.len(), shuffled.len());
     }
@@ -37,7 +57,7 @@ mod tests {
     #[test]
     fn should_return_a_shuffled_list_with_all_original_items() {
         let list = vec![1, 2, 3, 4, 5];
-        let shuffled = shuffle(&list, 100);
+        let shuffled = shuffle(&list);
 
         assert_eq!(list.len(), shuffled.len());
         
@@ -46,8 +66,19 @@ mod tests {
         }
     }
 
+    // test shuffling two times return different results
     #[test]
-    fn should_shuffle_large_arrays() {
+    fn should_return_different_results_when_shuffling_two_times() {
+        let list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        let shuffled1 = shuffle(&list);
+        let shuffled2 = shuffle(&list);
+
+        assert_ne!(shuffled1, shuffled2);
+    }
+
+    // test shiffling a big list is performant
+    #[test]
+    fn should_shuffle_array_with_50m_elements_in_under_25s() {
         let mut list = Vec::new();
         
         for i in 0..50000000 {
@@ -56,9 +87,9 @@ mod tests {
 
         let start_time = Instant::now();
 
-        shuffle(&list, 100);
+        shuffle(&list);
         
         let timeresult = start_time.elapsed();
-        assert!(timeresult < Duration::from_secs(10), "Shuffling took too long: {:?}", timeresult);
+        assert!(timeresult < Duration::from_secs(12), "Shuffling took too long: {:?}", timeresult);
     }
 }
